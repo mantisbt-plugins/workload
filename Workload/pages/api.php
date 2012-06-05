c2efb5fe9df39b226064e806cff77f21fd624419
unknown <rcasteran@.(none)>
2012-06-05 14:43:02 +0200

    - Change_1 : change function get_numeric_custom_field_ids() into get_custom_field_ids(CUSTOM_FIELD_TYPE_ENUM) - Change_2 : prevent redefinition of get_custom_field_ids(CUSTOM_FIELD_TYPE_ENUM)

--- Workload/pages/api.php
+++ Workload/pages/api.php
@@ -5,13 +5,16 @@
 	define(PLUGIN_WORKLOAD_PRINT_ISSUE_WORKLOAD_DEFAULT, '-1');
 	define(PLUGIN_WORKLOAD_PRINT_ISSUE_OVERLOAD_DEFAULT, '-1');
 	
+	if(!function_exists('get_custom_fied_ids'))
+	{
  	/**
-	 * Get the numeric custom field ids (if any)
+	 * Get the custom field ids of a specified type (if any)
 	 *
+	 * @param array of custom field type
 	 * @return array of custom field ids
 	 * @author rcasteran
 	 */
-	function get_numeric_custom_fied_ids() {
+	function get_custom_fied_ids($p_types) {
 		$t_custom_field_table = db_get_table( 'mantis_custom_field_table' );
 		$query = "SELECT *
 				  FROM $t_custom_field_table
@@ -22,12 +25,15 @@
 
 		for( $i = 0;$i < $t_row_count;$i++ ) {
 			$row = db_fetch_array( $result );
-			if($row['type'] == CUSTOM_FIELD_TYPE_NUMERIC) {
-				array_push( $t_ids, $row['id'] );
+			foreach($p_types as $t_type) {
+				if($row['type'] == $t_type) {
+					array_push( $t_ids, $row['id'] );
+				}
+				/* Else do nothing */
 			}
-			/* Else do nothing */
 		}
 
 		return $t_ids;
-	} /* End of get_numeric_custom_fied_ids() */
+	} /* End of get_custom_fied_ids() */
+	}
 ?>
